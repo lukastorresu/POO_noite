c1e46e92528163d2cb795908a5c3bba1e33b8cfe
lukastorresu <lukastorresu@gmail.com>
2025-04-28 20:01:33 -0300

    Polimorfismo

---
+++ PolimorfismoFuncionario/Funcionario.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PolimorfismoFuncionario
+{
+    public class Funcionario
+    {
+        public Funcionario(int codigo, string nome, double salario)
+        {
+            Codigo = codigo;
+            Nome = nome;
+            Salario = salario;
+        }
+        public virtual double CalcularBonificacao()[
+            return Salario * 0.1;
+        ]
+    }
+}
--- Revisao/obj/Debug/net8.0/Revisao.AssemblyInfo.cs
+++ Revisao/obj/Debug/net8.0/Revisao.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Revisao")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74abebbfa3698f74fd1a0a59522cb97b956cb668")]
 [assembly: System.Reflection.AssemblyProductAttribute("Revisao")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Revisao")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
